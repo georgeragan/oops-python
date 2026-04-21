4b6f9a0f4c23a3721fdb665fcafa24e8657488d6
Sravan <georgeragan123@gmail.com>
2026-04-21 11:45:14 +0530

    3 commit

--- oops2.py
+++ oops2.py
@@ -1,6 +1,6 @@
 class Chatbook:
     def __init__(self):
-        self.username=""
+        self.email=""
         self.password=""
         self.loggedin=False
         self.menu()
@@ -11,16 +11,40 @@ class Chatbook:
                          2-Press 2 to sign in
                          3-Press 3 to write a post
                          4-Press 4 to message a friend
-                         5-Press any other key to exit""")
+                         5-Press any other key to exit\t""")
         if user_input=="1":
-            pass
+            self.signup()
         elif user_input=="2":
-            pass
+            self.signin()
         elif user_input=="3":
             pass
         elif user_input=="4":
             pass
         else:
             exit()
+
+    def signup(self):
+        email=input("Enter you mail : ")
+        password=input("Enter your password : ")
+        self.email=email
+        self.password=password
+        print("Print logged in successfully")
+        self.menu()
+    
+    def signin(self):
+        if self.email=="" and self.password=="":
+            print("Signup first then sign in")
+        else:
+            user=input("Enter your mail here : ")
+            pwd=input("Enter your pwd here : ")
+            if self.email==user and self.password==pwd:
+                self.loggedin=True
+                print("Signed in success")
+            else:
+                print("Please enter correct credentials")
+        print("\n")
+        self.menu()
+        
+
 
 obj=Chatbook()
